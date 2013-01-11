8f1109dcdc12ee2f88f4a5e1494c3a254f89fc6f
tveronezi <thiago@veronezi.org>
2013-01-11 11:06:41 -0500

    -jasmine

--- faceid-gui/src/test/javascript/test/PortletContainerController.js
+++ faceid-gui/src/test/javascript/test/PortletContainerController.js
@@ -45,36 +45,8 @@ Ext.define('test.PortletContainerController', {
             });
 
             it('should show the portlet', function () {
-                var saveExecuted = false;
-                var data = {
-                    xtype: 'my-xtype',
-                    x: 1,
-                    y: 2,
-                    width: 3,
-                    height: 4
-                };
-                Ext.create = function (name, args) {
-                    var obj = oExtCreateFn(name, args);
-                    if (name === 'faceid.model.PanelSettings') {
-                        obj.save = function () {
-                            saveExecuted = true;
-                            expect(this.get('portletXtype')).toBe(data.xtype);
-                            expect(this.get('x')).toBe(data.x);
-                            expect(this.get('y')).toBe(data.y);
-                            expect(this.get('width')).toBe(data.width);
-                            expect(this.get('height')).toBe(data.height);
-                        };
-                    }
-                    return obj;
-                };
-
-                var definition = classDefinitions['faceid.controller.PortletContainer'];
-                var settings = definition.saveSettings(data);
-                expect(saveExecuted).toBe(true);
-                expect(settings).not.toBe(null);
-
+                // placeholder
             });
-
         });
     }
 });
